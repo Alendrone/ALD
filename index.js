5eb5a94dbbd123637b6c706ee89a471568656b1c
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-17 02:31:44 +0000

    vvv

---
+++ index.js
@@ -0,0 +1,11 @@
+var hmbr = document.getElementById("burger-menu"),
+mnu = document.getElementById("mobile-menu"),
+exit = document.getElementById("close-menu");
+
+hmbr.addEventListener("click",function () {
+  mnu.style.display = "flex";
+});
+
+exit.addEventListener("click",function () {
+  mnu.style.display = "none";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "secondary": {
+          "100": "#d2d6daff",
+          "200": "#8392abff",
+          "500": "#344767ff",
+          "600": "#252f40ff",
+          "700": "#20273fff"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "800": "#D9D9D9",
+          "900": "#D9D9D9",
+          "White": "#FFFFFF",
+          "Black": "#000000"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "tripleunit": "3rem",
+        "unitandahalfunit": "1.5rem",
+        "triplequarterunit": "0.75rem",
+        "FiveUnits": "80px",
+        "ThreeUnits": "48px",
+        "HalfUnit": "0.5rem",
+        "SixUnits": "6rem",
+        "TwoUnits": "32px",
+        "doubleunit": "2rem",
+        "Unit": "1rem",
+        "FourUnits": "4rem",
+        "OneAndHalfUnits": "24px"
+      },
+      "borderRadius": {
+        "radius40": "40px",
+        "Round": "50%",
+        "Radius4": "4px",
+        "radius75": "0.75rem",
+        "radius5": "0.5rem",
+        "Radius8": "8px",
+        "radius1": "1rem",
+        "Radius2": "2px"
+      },
+      "scale": {
+        "Medium": "96px",
+        "MaxWidth": "1400px",
+        "Small": "48px",
+        "XLarge": "192px",
+        "Large": "144px",
+        "XSmall": "16px",
+        "XXLarge": "288px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.html"
+  ]
+}
