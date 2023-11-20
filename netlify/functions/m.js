eef0c8535e0036671891bf2f74a1e020a905fa9d
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-19 21:24:04 -0600

    update netlify/functions/m.js

--- netlify/functions/m.js
+++ netlify/functions/m.js
@@ -1,14 +1,14 @@
 import { Resend } from 'resend';
 import { Config, Context } from "@netlify/edge-functions";
 
-export default async (req: Request, context: Context) => {
+export default async (req, context) => {
    // const {name,email,address,phone,message} = JSON.parse(event.body),
   const data = await req.text();
   await processData(data);
   return new Response(`${data}`);
 }
 
-export const config: Config = {
+export const config = {
   method: "POST",
   path: "/m"
 }
