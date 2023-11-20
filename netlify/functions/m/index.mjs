699db52dbac58f2afb6d07760005ef780245d356
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-20 06:56:35 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -3,12 +3,12 @@ import { Config, Context } from "@netlify/functions";
 
 export default async (req, context) => {
    //const {name,email,address,phone,message} = JSON.parse(event.body),
-  const data = await req.text();
+  const data = await req;
  // await processData(data);
   return new Response(`Sucess!`);
 }
 
 export const config = {
 method: "POST"
-//  path: "/m"
+//path: "/contact"
 }
