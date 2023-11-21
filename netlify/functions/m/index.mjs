f36e418e56adbbc3efe2172124187a27c0e9bab7
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-21 03:46:30 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -1,6 +1,13 @@
 //import { Resend } from 'resend';
 import { Config, Context } from "@netlify/functions";
 
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Value` }),
+  };  
+};
+
 export default async (req) => {
   return new Response("Hello, world!");
 }
