079cc2f56280dc8c453b6c0472b951868618f717
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-21 03:17:52 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -1,6 +1,10 @@
 //import { Resend } from 'resend';
 import { Config, Context } from "@netlify/functions";
 
+export default async (req, context) => {
+  return new Response("Hello, world!");
+};
+
 //export const config = {
 //method: "GET"
 //path: "/contact"
