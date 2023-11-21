d332a34400e23dba0f5473887a270284b8c26c16
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-21 04:36:19 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -2,7 +2,7 @@
 //import { Config, Context } from "@netlify/functions";
 
 exports.handler = async function (event, context) {
-  var e = JSON.stringify(event.body);
+  var e = JSON.stringify(event);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: `${e}` }),
