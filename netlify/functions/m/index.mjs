c5c10bcd682f4905922c4de7e0f0c59cdc04b6f8
Alendrone <33103761+Alendrone@users.noreply.github.com>
2023-11-21 06:13:35 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -2,7 +2,25 @@
 //import { Config, Context } from "@netlify/functions";
 
 exports.handler = async function (event, context) {
-  var e = JSON.stringify(event);
+  var body = JSON.stringify(event.body),
+  submit = {},
+  params = body.split("&"),
+  i = params.length;
+  
+  for (;i;--i) {
+    var [key, value] = params[i - 1].split("="),
+    decodedKey = decodeURIComponent(key),
+    decodedValue = decodeURIComponent(value);
+    
+    if (submit.hasOwnProperty(decodedKey)) {
+      if (!Array.isArray(submit[decodedKey])) {
+        submit[decodedKey] = [submit[decodedKey]];
+    }
+      
+    submit[decodedKey].push(decodedValue);
+    } else submit[decodedKey] = decodedValue;
+ }
+  
   return {
     statusCode: 200,
     body: JSON.stringify({ message: `${e}` }),
