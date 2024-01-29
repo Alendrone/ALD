687acd8cadab0d2fadbd89a6a25e502674d6fc68
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 14:37:19 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var hmbr = document.getElementById("burger-menu"),
 mnu = document.getElementById("mobile-menu"),
 exit = document.getElementById("close-menu");
+mnu.style.opacity = "0";
+mnu.style.visibility = "hidden";
+mnu.style.display = "flex";
 
 hmbr.addEventListener("click",function () {
   mnu.style.animationName =  "popin";
