c0ad796efc5be329d4b38e94b1dbb8a74ada9bf2
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 13:56:22 -0600

    update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ exit = document.getElementById("close-menu");
 
 hmbr.addEventListener("click",function () {
   mnu.style.display = "flex";
-  mnu.style.animation = "pop-in 1180ms ease-in 0 1";
+  mnu.style.animation = "pop-in 1180ms ease-in 0s 1";
 });
 
 exit.addEventListener("click",function () {
   mnu.style.display = "none";
-  mnu.style.animation = "pop-in 1180ms ease-out 0 1 reverse";
+  mnu.style.animation = "pop-in 1180ms ease-out 0s 1 reverse";
 });
