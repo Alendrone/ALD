482c46b07404e3f81c80f57f33392c553202c496
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 14:59:37 -0600

    update index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ exit = document.getElementById("close-menu");
 
 hmbr.addEventListener("click",function () {
   mnu.style.display = "flex";
+  mnu.focus();
 });
 
 exit.addEventListener("click",function () {
+  mnu.focus();
   mnu.style.opacity = "0";
 });
