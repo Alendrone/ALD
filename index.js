0a809b3facb387f91ffcaaceb1481e7ef3b91aed
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 15:06:27 -0600

    update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ mnu = document.getElementById("mobile-menu"),
 exit = document.getElementById("close-menu");
 
 hmbr.addEventListener("click",function () {
+  mnu.style.display = "flex";
   mnu.focus();
   mnu.addEventListener("animationstart",function(){mnu.style.display = "flex";},{once:true});
 });
