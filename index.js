0cac6794eb84271448df8881e1dc0f715808e654
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 15:03:48 -0600

    update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ mnu = document.getElementById("mobile-menu"),
 exit = document.getElementById("close-menu");
 
 hmbr.addEventListener("click",function () {
-  mnu.style.display = "flex";
   mnu.focus();
+  mnu.addEventListener("animationstart",function(){mnu.style.display = "flex";},{once:true});
 });
 
 exit.addEventListener("click",function () {
   mnu.focus();
-  mnu.style.opacity = "0";
+  mnu.addEventListener("animationend",function(){mnu.style.display = "none";},{once:true});
 });
