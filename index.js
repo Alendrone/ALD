e3f591fa457d8ae147a37754693df25880c529c0
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 14:53:40 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 var hmbr = document.getElementById("burger-menu"),
 mnu = document.getElementById("mobile-menu"),
 exit = document.getElementById("close-menu");
-mnu.style.opacity = "0";
-mnu.style.visibility = "hidden";
-mnu.style.display = "flex";
 
 hmbr.addEventListener("click",function () {
-  mnu.classList.add("intraexo");
-  mnu.style.visibility = "visible";
-  mnu.style.opacity = "1";
-  //mnu.style.display = "flex";
+  mnu.style.display = "flex";
 });
 
 exit.addEventListener("click",function () {
-  mnu.classList.add("intraexo");
-  mnu.style.visibility = "hidden";
   mnu.style.opacity = "0";
-  //mnu.style.display = "none";
 });
