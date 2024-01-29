2330c8e9092af1bcc3cba243544fecd514d8321b
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-29 14:00:04 -0600

    update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ mnu = document.getElementById("mobile-menu"),
 exit = document.getElementById("close-menu");
 
 hmbr.addEventListener("click",function () {
-  mnu.style.display = "flex";
   mnu.style.animation = "pop-in 1180ms ease-in 0s 1";
+  mnu.style.display = "flex";
 });
 
 exit.addEventListener("click",function () {
