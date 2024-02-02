10b3041b417de0661787d2bc3115e220d490b592
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-02-02 08:15:20 -0600

    update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exit.addEventListener("click",function () {
 
 if (uri.pathname === "/contact") {
 var captcha = document.getElementById("captcha");
-hcaptcha.render({captcha,"site-key":"a3524c06-9e6e-4856-a31b-98e565f80c78"});
+hcaptcha.render("captcha",{"site-key":"a3524c06-9e6e-4856-a31b-98e565f80c78"});
 captcha.addEventListener("verified", function (e) {
   console.log("verified event", {token: e.token});
 });
