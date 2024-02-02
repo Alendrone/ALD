b3da28ca94ffa34a3b4fe141d6b6c85cd4367df4
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-02-02 07:03:50 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var hmbr = document.getElementById("burger-menu"),
 mnu = document.getElementById("mobile-menu"),
-exit = document.getElementById("close-menu");
+exit = document.getElementById("close-menu"),
+uri = new URL(window.location.href);
 
 hmbr.addEventListener("click",function () {
   mnu.style.display = "flex";
@@ -9,3 +10,14 @@ hmbr.addEventListener("click",function () {
 exit.addEventListener("click",function () {
   mnu.style.display = "none";
 });
+
+if (uri.pathname === "/contact") {
+var captcha = document.getElementById("captcha");
+
+captcha.addEventListener("verified", function (e) {
+  console.log("verified event", {token: e.token});
+});
+captcha.addEventListener("error", function (e) {
+  console.log("error event", {error: e.error});
+});
+}
