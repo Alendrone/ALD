b524ad8350f7663e2669d9598c3aafd7768cf868
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-02-04 08:43:45 -0600

    update netlify/functions/m/index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -1,4 +1,5 @@
 import {Resend} from "resend";
+import {textGeneration} from "@huggingface/inference";
 
 exports.handler = async function (event, context) {
   var body = event.body,
@@ -6,13 +7,47 @@ exports.handler = async function (event, context) {
   uid = Math.floor(Date.now() / 1000).toString(),
   submit = [],
   params = body.split("&"),
-  i = params.length;
+  i = params.length,
+  len,
+  nth,
+  cur,
+  ndx,
+  rawdata;
   
  for (;i;--i) {
     var cur = i - 1;
     params[cur] = params[cur].split("=");
     submit[cur] = decodeURIComponent(params[cur][1]);
     if (submit[cur].indexOf("+") + 1) submit[cur] = submit[cur].split("+").join(" ");
+ }
+ try {
+   await textGeneration({
+     accessToken: process.env.TOKEN,
+     model: "mistralai/Mistral-7B-Instruct-v0.2",
+     inputs:`Perform a analysis on the text: "${submit[4]}", using just one number and determine its magnitude factoring in the text's meaning by comparing and contrasting whether it sounds like a suspicious/spam/scam/phishing/social engineering attempt (closer to zero), potentially suspicious/spam/scam/phishing/social engineering (closer to the mid-range), or not suspicious/spam/scam/phishing/social engineering (closer to nine) from one out of ten.\nFirst I require you MUST give me your best guess of the number from 0-9 and then your explanation after; no exceptions.`,
+     parameters:{
+       "temperature": 0.1,
+       "max_new_tokens": 4096
+   }}).then(function (resp) {
+     rawdata = resp.generated_text.split("\n")[3];
+   });
+
+
+   for (nth = rawdata.length;nth;--nth) {
+     cur = rawdata.charCodeAt(nth - 1) - 48;
+     if (10 > cur && cur >= 0) break;
+   }
+
+   if (cur < 5) throw null;
+ }
+ catch (err) {
+   return {
+      statusCode:400,
+      headers: {
+        "Content-Type": "text/html"
+      },
+      body:`<!doctype html><html><head><title>Under review.</title><link href=../../fonts.css rel=stylesheet></head><body><style>:root{font-size:1px}body,html{background:#000;font-size:35rem;font-family:Brisa Sans}.container{padding:0;min-width:auto;margin:0 -50% 0 -50%;width:100%;max-height:100vh;max-width:100vw;display:block;position:absolute;inset:0 50% 0 50%;box-sizing:content-box;background:#000}@media (min-height:626px) and (min-width:365px){.container{font-size:20rem}}@media (min-height:912px) and (min-width:540px){.container{font-size:40rem}.container{font-size:60rem}}@media (min-width:992px) and (min-height:654px){.container{font-size:100rem}}@media (min-width:1363px) and (min-height:559px){.container{font-size:120rem}}@media (min-width:1932px) and (min-height:1121px){.container{font-size:150rem}}.wrapper{line-height:1.5em;letter-spacing:.075em;right:100%;margin:50% 0 0 0;color:#fff;left:0;width:100%;padding:0;position:absolute;box-sizing:border-box;display:flex;text-align:center;justify-content:center;align-content:center;object-position:center;align-items:center}svg{width:5em}</style><div class=container><div class=wrapper style="margin:0 auto"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="#808080" fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/></svg></div><div class=wrapper><h2 style=color:#808080>Pending.</h2></div><br><br><br><div class=wrapper><p><span style=font-style:italic>Suspicous activity detected.</span><br/>Your email is sent but is pending for review.</div></div></body></html>`,
+   };
  }
  try {
    if (!event.body.length) throw null;
