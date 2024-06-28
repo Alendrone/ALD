580ff6aeaf18614bd89da300735774acc14ae2b0
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-06-28 09:01:00 -0500

    update payment.js

--- payment.js
+++ payment.js
@@ -63,7 +63,7 @@ function formatCurrency(input, blur) {
         input_val = "US$ " + input_val;
 
         // final formatting
-        if (blur === "blur" && input_val.length === 4) input_val += "0.50";
+        if (blur === "blur" && input_val.length === 4) input_val += "00.50";
         else if (blur === "blur") input_val += ".00";
     }
     /*for (i = usd.length;i;--i) {
