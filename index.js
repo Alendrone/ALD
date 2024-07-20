bcfb46fb46b76706482ec18f1a7935ce7a943ca1
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-20 18:09:13 -0500

    update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ rqid = false;
 async function submission(e) {
   e.preventDefault();
   if (uri.pathname === "/contact") {
-    var subscribe = document.getElementById("mailchimp"),
+    /*var subscribe = document.getElementById("mailchimp"),
     mail = formdata.get("email"),
     query = new URLSearchParams({
       email: mail
@@ -17,7 +17,7 @@ async function submission(e) {
         method:"POST",
         url:`https://arborlifedesigns.com/subscribe`,
         params:query
-    });
+    });*/
   }
   if (rqid) e.currentTarget.submit();
 }
