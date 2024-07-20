69ed9560b9161fdc7d49f9492f2b249952ed8184
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-19 22:35:40 -0500

    update netlify/subscribe/index.mjs

--- netlify/subscribe/index.mjs
+++ netlify/subscribe/index.mjs
@@ -1,19 +1,30 @@
-import axios from "axios"
+import axios from "axios";
 
 exports.handler = async function (event, context) {
-  const { email } = event.body,
-  enroll = {
-    members: [
-      {
-        email_address: email,
-        status: 'subscribed'
+  try {
+    const { email } = event.body,
+    enroll = {"members": [{"email_address": email,"status": "subscribed"}]},
+    res = await axios.post(`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`, JSON.stringify(enroll), {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.MAILCHIMP_KEY}`
       }
-    ]
-  };
-
-  const { status } = await axios.post(`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`, enroll, {
-    headers: {
-      Authorization: `apikey ${process.env.MAILCHIMP_KEY}`
-    }
-  });
+    });
+    return {
+      statusCode:200,
+      headers: {
+        "Content-Type": "text/plain"
+      },
+      body:`Succeeded!`
+    };
+  }
+  catch (err) {
+    return {
+      statusCode:400,
+      headers: {
+        "Content-Type": "text/plain"
+      },
+      body:`Failed with a error of ${err}`
+    };
+  }
 };
