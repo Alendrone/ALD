2e3f12cd0393ed82dd6be89e8e97759506c585b8
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-19 20:58:56 -0500

    update netlify/functions/subscribe/index.mjs

--- netlify/functions/subscribe/index.mjs
+++ netlify/functions/subscribe/index.mjs
@@ -3,14 +3,7 @@ import axios from "axios";
 exports.handler = async function (event, context) {
   try {
     const { email } = event.body,
-    enroll = {
-      members: [
-        {
-          email_address: email,
-          status: 'subscribed'
-        }
-        ]
-    },
+    enroll = {"members": [{"email_address": email,"status": "subscribed"}]},
     res = await axios.post(`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`, enroll, {
       headers: {
         "Content-Type": "application/json",
