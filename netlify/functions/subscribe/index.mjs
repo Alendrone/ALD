99ce3a6fd4b467e1607197b0c16999a4e0f76cea
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-20 01:05:00 -0500

    update netlify/functions/subscribe/index.mjs

--- netlify/functions/subscribe/index.mjs
+++ netlify/functions/subscribe/index.mjs
@@ -3,17 +3,20 @@ import axios from "axios";
 exports.handler = async function (event, context) {
   try {
   const { email } = event.body;
-  await axios({
+  
+  const json = {"members": [{"email_address": email,"status": "subscribed"}]};
+  
+  /*await axios({
     method:"POST",
     url:`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`,
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${process.env.MAILCHIMP_KEY}`
     },
-    data:{"members": [{"email_address": email,"status": "subscribed"}]},
+    data:,
     responseType:"json",
     responseEncoding:"utf8"
-  });
+  });*/
   
     return {
       statusCode:200,
