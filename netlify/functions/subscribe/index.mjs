9aa478c9c47bca406646828aaf9f8615f729ed55
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-19 20:28:22 -0500

    update netlify/functions/subscribe/index.mjs

--- netlify/functions/subscribe/index.mjs
+++ netlify/functions/subscribe/index.mjs
@@ -10,9 +10,10 @@ exports.handler = async function (event, context) {
           status: 'subscribed'
         }
         ]
-    };
+    },
+    encoded = btoa(process.env.LIST_ID);
     
-    await axios.post(`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`, enroll, {
+    await axios.post(`https://us4.api.mailchimp.com/3.0/lists/encoded`, enroll, {
       headers: {
         Authorization: `Bearer ${process.env.MAILCHIMP_KEY}`
       }
