9fbc383c10ee4fc91b419542b7401c38fda68c26
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-19 23:29:13 -0500

    update netlify/subscribe/index.mjs

--- netlify/subscribe/index.mjs
+++ netlify/subscribe/index.mjs
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 exports.handler = async function (event, context) {
-  try {
-    const { email } = event.body,
-    res = await axios({
-        method:"POST",
-        url:`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.MAILCHIMP_KEY}`
-        },
-        data:{"members": [{"email_address": email,"status": "subscribed"}]},
-        responseType:"json",
-        responseEncoding:"utf8"
-    });
-    
+  const { email } = event.body,
+  {status} = await axios({
+    method:"POST",
+    url:`https://us4.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.MAILCHIMP_KEY}`
+    },
+    data:{"members": [{"email_address": email,"status": "subscribed"}]},
+    responseType:"json",
+    responseEncoding:"utf8"
+  });
+  
+  
+  if (status) {
     return {
       statusCode:200,
       headers: {
@@ -23,7 +24,7 @@ exports.handler = async function (event, context) {
       body:`Succeeded!`
     };
   }
-  catch (err) {
+  /*catch (err) {
     return {
       statusCode:400,
       headers: {
@@ -31,5 +32,5 @@ exports.handler = async function (event, context) {
       },
       body:`Failed with a error of ${err}`
     };
-  }
+  }*/
 };
