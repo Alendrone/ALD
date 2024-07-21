fd1f8b9ef1e59d166cb284491ecd78ad91e9e475
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-07-20 19:54:49 -0500

    update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function subscribe() {
 function submission(e) {
   e.preventDefault();
   if (uri.pathname === "/contact") subscribe();
-  if (rqid) e.currentTarget.submit();
+  //if (rqid) e.currentTarget.submit();
 }
 hmbr.addEventListener("click",function () {
   mnu.style.display = "flex";
