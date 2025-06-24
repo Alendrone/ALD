b1f9b9e8268e8f51afe484521b6adeab13e46443
Alendrone <33103761+Alendrone@users.noreply.github.com>
2025-06-24 07:33:08 -0500

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function subscribe() {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({token:rqid}),
+    body: JSON.stringify({response:rqid}),
   };
   fetch("/subscribe?email=" + mail, options)
   .then(response => response.json())
