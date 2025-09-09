6e270213fe868268b80d943674f84ed9453028ef
Alendrone <33103761+Alendrone@users.noreply.github.com>
2025-09-09 04:03:47 -0700

    Update index.mjs

--- netlify/functions/m/index.mjs
+++ netlify/functions/m/index.mjs
@@ -3,7 +3,7 @@ import {InferenceClient} from "@huggingface/inference";
 import fetch from "node-fetch";
 import queryString from "query-string";
 
-exports.handler = async function (event, context) {
+export async function (event, context) {
   var body = event.body,
   resend = new Resend(process.env.PASSPHRASE),
   uid = Math.floor(Date.now() / 1000).toString(),
